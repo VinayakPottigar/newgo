3533ae4c99d278ec278e54fa03eb36a1b36bca58
VinayakPottigar <vinayak.javadeveloper@gmail.com>
2023-09-13 16:51:05 +0530

    Activity added in activity branch

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.calculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSubmit=findViewById(R.id.button4);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gonext = new Intent(getApplicationContext(),Second_Activity.class);
+                startActivity(gonext);
+            }
+        });
     }
 }
